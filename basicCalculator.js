a3e8449b150671ff1dbfac4575e12d0a21c633b2
Mohamed-AboRahma <aborahma5122005@gmail.com>
2024-04-24 14:02:44 +0200

    update

--- basicCalculator.js
+++ basicCalculator.js
@@ -2,17 +2,31 @@ const inputEquation = document.querySelector(".simpleCalculator .equation");
 const finalResult = document.querySelector(".simpleCalculator #enteryEquation");
 const allButtons = Array.from(
   document.querySelector(".simpleCalculator .buttons").children
-).forEach((ele) => {
-  ele.addEventListener("click", (e) => {
-    if (e.target.className == "equal") {
-      inputEquation.innerText == "";
-      finalResult.value = eval(inputEquation.innerText);
-      inputEquation.innerText = "";
-    } else if (e.target.className == "clear") {
-      inputEquation.innerText = "";
-      finalResult.innerText = "";
-    } else {
-      inputEquation.innerText += e.target.innerText;
+);
+allButtons.forEach((ele) => {
+  ele.addEventListener("click", async (e) => {
+    try {
+      e.target.className == "equal"
+        ? inputEquation.innerText == ""
+          ? null
+          : ((finalResult.value = eval(inputEquation.innerText)),
+            (inputEquation.innerText = ""))
+        : e.target.className == "clear"
+        ? ((inputEquation.innerText = ""), (finalResult.value = ""))
+        : (inputEquation.innerText += e.target.innerText);
+    } catch (error) {
+      inputEquation.classList.add("error");
+      inputEquation.innerText = error;
+      allButtons.forEach((ele) => {
+        ele.setAttribute("disabled", true);
+      });
+      setTimeout(() => {
+        inputEquation.innerText = "";
+        inputEquation.classList.remove("error");
+        allButtons.forEach((ele) => {
+          ele.removeAttribute("disabled");
+        });
+      }, 3000);
     }
   });
 });
